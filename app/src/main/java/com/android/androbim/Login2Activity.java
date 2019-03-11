230d671ae9eca3ec8b4df99c6d52f549f8c6031c
AVEIT <madhu8580@gmail.com>
2019-03-11 19:26:46 +0530

    // login2

--- app/src/main/java/com/android/androbim/Login2Activity.java
+++ app/src/main/java/com/android/androbim/Login2Activity.java
@@ -1,13 +1,28 @@
 package com.android.androbim;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class Login2Activity extends AppCompatActivity {
+
+    private Button btnSignIn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login2);
+
+        btnSignIn = findViewById(R.id.btnLogin2);
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent iSign = new Intent(getApplicationContext(), VerifyEmailActivity.class);
+                startActivity(iSign);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/android/androbim/VerifyEmailActivity.java
@@ -0,0 +1,14 @@
+package com.android.androbim;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class VerifyEmailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_verify_email);
+
+    }
+}
