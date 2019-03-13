d102387c5672d1567a76827a6f452ea464a2be1a
madhu <madhu8580@gmail.com>
2019-03-13 18:25:37 +0530

    // registration

---
+++ app/src/main/java/com/android/androbim/relativelayout/RelativelayoutExample.java
@@ -0,0 +1,16 @@
+package com.android.androbim.relativelayout;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.android.androbim.R;
+
+public class RelativelayoutExample extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_relativelayout);
+    }
+}
